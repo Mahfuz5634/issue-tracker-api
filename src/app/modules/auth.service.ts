bf18d223c363a5055ef8f5e98dfdebd5d563fce7
Mahfuz <mahfuzru5634@gmail.com>
2026-05-22 23:55:47 +0600

    working on auth

---
+++ src/app/modules/auth.service.ts
@@ -0,0 +1,61 @@
+import { db } from "../config/db";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SALT_ROUND=Number(process.env.BCRYPT_SALT_ROUNDS);
+const JWT_SECRET=process.env.JWT_SECRET as string;
+
+export const authService={
+    async signup(playload:any){
+        const hashedPassword= await bcrypt.hash(playload.password,SALT_ROUND);
+
+        const result = await db.query(
+            `
+            INSERT INTO users(name,email,password,role)
+            VALUES($1,$2,$3,$4)
+            RETURNING id,name,email,role,created_at,updated_at `,
+            [
+                playload.name,
+                playload.email,
+                hashedPassword,
+                playload.role || 'contributor',
+            ]
+        );
+        return result.rows[0];
+    },
+
+    async login(playload:any){
+        const userRes= await db.query(
+            `SELECT * FROM users WHERE email= $1
+            `,
+            [playload.email]
+        )
+        const user=userRes.rows[0];
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+        const  isMatch= await bcrypt.compare(playload.password,user.password);
+
+        if(!isMatch){
+             throw new Error("Invalid credentials");
+        }
+        const token= jwt.sign({
+            id:user.id,
+            name:user.name,
+            role:user.role,
+        },
+        JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES_IN || "7d"}
+    );
+    return {
+        token,
+        user:{
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            role:user.role,
+            created_at:user.created_at,
+            updated_at: user.updated_at,
+        },
+    };
+    },
+};
---
+++ src/app/modules/auth.validate.ts
@@ -0,0 +1,15 @@
+
+import {z} from "zod";
+
+export const signupSchema = z.object({
+    name: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(6),
+    role: z.enum(["contributor","maintainer"]).optional(),
+
+});
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password:z.string(),
+});
